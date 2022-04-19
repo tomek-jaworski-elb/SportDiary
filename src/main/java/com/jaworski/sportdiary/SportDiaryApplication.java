31f61e466d64f890e6c689ca05237fa10678f3a4
Tomek Jaworski <tj.toja@gmail.com>
2022-04-19 18:08:10 +0200

    Initial commit

---
+++ src/main/java/com/jaworski/sportdiary/SportDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.jaworski.sportdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaworski/sportdiary/SportDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaworski.sportdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

964d730315f665ad7a20fe1e279b6f3990f1961e
Tomek Jaworski <tj.toja@gmail.com>
2022-04-28 17:48:18 +0200

    Moved functionality for user

--- src/main/java/com/jaworski/sportdiary/controller/AuthorizedController.java
+++ src/main/java/com/jaworski/sportdiary/controller/AuthorizedController.java
@@ -3,12 +3,12 @@ package com.jaworski.sportdiary.controller;
 import com.jaworski.sportdiary.domain.Activity;
 import com.jaworski.sportdiary.service.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -18,15 +18,50 @@ public class AuthorizedController {
     @Autowired
     private ActivityRepository activityRepository;
 
-    @GetMapping("")
-    public String main() {
-        return "user";
-    }
-
-    @GetMapping(path = {"/list"})
+    @GetMapping(path = {"/list", "/", ""})
     public String list(Model model) {
         List<Activity> list = activityRepository.getRepository();
         model.addAttribute("activities", list);
-        return "index";
+        return "list";
+    }
+
+    @PostMapping(path = "/add")
+    public String newActivity(@Valid @ModelAttribute Activity activity, Model model) {
+        System.out.println(activity);
+        activityRepository.addActivity(activity);
+        model.addAttribute("activity", activity.toString());
+        return "new";
+    }
+
+    @GetMapping(value = "/delete", params = "id")
+    public RedirectView delete(@RequestParam(required = true, defaultValue = "", name = "id") int id) {
+        activityRepository.delete(id);
+        return new RedirectView("/user");
+    }
+
+    @GetMapping("/save")
+    public RedirectView saveList(Model model) {
+        activityRepository.saveToJson();
+        return new RedirectView("/user");
+    }
+
+    @GetMapping(path = "/more", params = "id")
+    public String more(@RequestParam int id, Model model) {
+
+        model.addAttribute("activity", activityRepository.getActivities().stream()
+                .filter(activity -> activity.getId() == id)
+                .findFirst()
+                .orElse(new Activity()));
+        return "more";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        System.out.println("***");
+        Activity activity = new Activity();
+        int nextId = activityRepository.maxId() + 1;
+        activity.setId(nextId);
+        model.addAttribute("activity", activity);
+        return "add";
     }
 }
--- src/main/java/com/jaworski/sportdiary/controller/IndexController.java
+++ src/main/java/com/jaworski/sportdiary/controller/IndexController.java
@@ -6,12 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -27,51 +22,9 @@ public class IndexController {
         return "welcome";
     }
 
-    @GetMapping("/add")
-    public String add(Model model) {
-        Activity activity = new Activity();
-        int nextId = activityRepository.maxId() + 1;
-        activity.setId(nextId);
-        model.addAttribute("activity", activity);
-        return "add";
-    }
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
-
-    @PostMapping("/add")
-    public String newActivity(@Valid @ModelAttribute Activity activity, Model model) {
-        activityRepository.addActivity(activity);
-        model.addAttribute("activity", activity.toString());
-        return "new";
-    }
-
-    @GetMapping("/save")
-    public String saveList(Model model) {
-        activityRepository.saveToJson();
-        model.addAttribute("activities", activityRepository.getActivities());
-        return "index";
-    }
-
-    @GetMapping("/more")
-    public String more(@RequestParam int id, Model model) {
-
-        model.addAttribute("activity", activityRepository.getActivities().stream()
-                .filter(activity -> activity.getId() == id)
-                .findFirst()
-                .orElse(new Activity()));
-        return "more";
-    }
-
-    @GetMapping(value = "/delete", params = "id")
-    public RedirectView delete(@RequestParam(required = true, defaultValue = "", name = "id") int id) {
-        activityRepository.delete(id);
-        return new RedirectView("/");
-    }
-//    @PostMapping("/login_perform")
-//    public RedirectView loginPerform() {
-//        return new RedirectView("/");
-//    }
 }

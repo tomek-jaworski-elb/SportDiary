b26f3d1e1b6765a13021660e5a05bc3dacd2d2e6
Tomek Jaworski <tj.toja@gmai.com>
2022-05-26 12:14:42 +0200

    Added scheduled task

---
+++ src/main/java/com/jaworski/sportdiary/config/ScheduleConfig.java
@@ -0,0 +1,23 @@
+package com.jaworski.sportdiary.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+@EnableScheduling
+@EnableAsync
+public class ScheduleConfig {
+
+    @Scheduled(cron = "*/60 * * * * *")
+    @Async
+    public void scheduleFixedRateTask() {
+        System.out.println(
+                "Run method every 1 minute: " +  LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+}

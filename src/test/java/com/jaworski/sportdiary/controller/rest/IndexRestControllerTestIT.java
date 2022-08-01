1cd5ec25b9b8505b28b657e70e11ca0cb4922552
Tomek Jaworski <tj.toja@gmail.com>
2022-08-01 16:21:10 +0200

    Integetion test implemented

--- src/test/java/com/jaworski/sportdiary/controller/rest/IndexRestControllerTestIT.java
+++ src/test/java/com/jaworski/sportdiary/controller/rest/IndexRestControllerTestIT.java
@@ -1,35 +1,45 @@
 package com.jaworski.sportdiary.controller.rest;
 
-import com.jaworski.sportdiary.domain.Activity;
-import com.jaworski.sportdiary.service.activity.ActivityService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @MockBean(RestTemplate.class)
 @WebMvcTest
-class IndexRestControllerTest {
+@ContextConfiguration(classes = {IndexRestController.class})
+class IndexRestControllerTestIT {
 
-    private static final String BASE_URL = "/proxy/learners";
-    @MockBean
+    private static final String BASE_URL = "/api";
+    @MockBean()
     private IndexRestController indexRestController;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void getAllActivity() {
-        given(indexRestController.getAllActivities()).willReturn(ResponseEntity.ok(new ArrayList<Activity>()));
+    void whenContextLoads_thenServiceIsInjected() {
+        Assertions.assertNotNull(indexRestController);
+    }
+
+    @Test
+    void getAllActivity() throws Exception {
+        given(indexRestController.getAllActivities()).willReturn(ResponseEntity.ok(new ArrayList<>()));
+
+        mockMvc.perform(get(BASE_URL + "/acts").accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpectAll(status().is4xxClientError());
     }
 }

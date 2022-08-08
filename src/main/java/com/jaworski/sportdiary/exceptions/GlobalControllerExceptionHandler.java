0c1c262012a19ab27d2dca05560f26092050e880
Tomek Jaworski <tj.toja@gmail.com>
2022-08-08 16:14:01 +0200

    Cleanup code

--- src/main/java/com/jaworski/sportdiary/exceptions/GlobalControllerExceptionHandler.java
+++ src/main/java/com/jaworski/sportdiary/exceptions/GlobalControllerExceptionHandler.java
@@ -12,18 +12,19 @@ import org.springframework.web.client.RestClientResponseException;
 @ControllerAdvice
 class GlobalControllerExceptionHandler {
     private static final Logger LOGGER = LogManager.getLogger(GlobalControllerExceptionHandler.class);
+    public static final String MESSAGE = "Exception: {}";
 
     @ResponseStatus(code = HttpStatus.NOT_FOUND)  // 409
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleConflict(Exception e) {
-        LOGGER.error("Exception: " + e.getMessage());
+        LOGGER.error("Exception: {} ", e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         // Nothing to do
     }
 
     @ExceptionHandler(RestClientResponseException.class)
     public ResponseEntity<String> handleException(RestClientResponseException e) {
-        LOGGER.error("Exception: " + e.getMessage());
+        LOGGER.error(MESSAGE, e.getMessage());
         return ResponseEntity.status(e.getRawStatusCode())
                 .body(e.getResponseBodyAsString());
         // Nothing to do
@@ -31,21 +32,21 @@ class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        LOGGER.error("Exception: " + e.getMessage());
+        LOGGER.error(MESSAGE, e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         // Nothing to do
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
-        LOGGER.error("Exception: " + e.getMessage());
+        LOGGER.error(MESSAGE, e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         // Nothing to do
     }
 
     @ExceptionHandler(IllegalAccessException.class)
     public ResponseEntity<String> handleIllegalAccessException(IllegalAccessException e) {
-        LOGGER.error("Exception: " + e.getMessage());
+        LOGGER.error(MESSAGE, e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         // Nothing to do
     }

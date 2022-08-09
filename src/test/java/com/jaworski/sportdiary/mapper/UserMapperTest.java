2bb834b4ee2d85a7ac3f19a9d871d2eb2baa3503
Tomek Jaworski <tj.toja@gmail.com>
2022-08-09 19:46:20 +0200

    JT EntityUser to User

--- src/test/java/com/jaworski/sportdiary/mapper/UserMapperTest.java
+++ src/test/java/com/jaworski/sportdiary/mapper/UserMapperTest.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class UserMapperTest {
@@ -19,16 +20,31 @@ class UserMapperTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Test
+    void toUserEntity_throwsIllegalArgumentException_givenUserDefaultConstructor() {
+        User user = new User();
+        assertThat(user).isNotNull();
+        assertThat(user.getFirstName()).isNull();
+        assertThat(user.getLastName()).isNull();
+        assertThat(user.getEmail()).isNull();
+        assertThat(user.getPassword()).isNull();
+        assertThat(user.getRoles()).isNull();
+        assertThrows(IllegalArgumentException.class, () -> userMapper.toUserEntity(user));
+    }
+
 
     @Test
     void toUserEntity_returnsUserEntity_givenUser() {
+        // given
         User user = new User();
         user.setFirstName("Jan");
         user.setLastName("Kowalski");
         user.setEmail("toja@wp.pl");
         user.setPassword("haslo");
         user.setRoles("USER");
+        // when
         UserEntity userEntity = userMapper.toUserEntity(user);
+        // then
         assertThat(userEntity.getId()).isNull();
         assertThat(userEntity.getFirstName()).isEqualTo(user.getFirstName());
         assertThat(userEntity.getEmail()).isEqualTo(user.getEmail());
@@ -37,7 +53,22 @@ class UserMapperTest {
     }
 
     @Test
-    void toUser() {
-
+    void toUser_returnsUser_givenUserEntity() {
+        // given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(UUID.randomUUID());
+        userEntity.setFirstName("Jan");
+        userEntity.setEmail("toja@wp.pl");
+        userEntity.setPassword(passwordEncoder.encode("haslo"));
+        userEntity.setRoles("USER");
+        userEntity.setActive(true);
+        // when
+        User user = userMapper.toUser(userEntity);
+        // then
+        assertThat(user.getId()).isEqualTo(userEntity.getId());
+        assertThat(user.getFirstName()).isEqualTo(userEntity.getFirstName());
+        assertThat(user.getEmail()).isEqualTo(userEntity.getEmail());
+        assertThat(user.getRoles()).isEqualTo(userEntity.getRoles().stream().reduce("", (s, r) -> s + "," + r));
+        assertThat(user.getPassword()).isEqualTo(userEntity.getPassword());
     }
 }
